b6d422d14e26ef4e1111970da257dadac03a7521
Orla Madden <omadden2@gmail.com>
2020-01-26 18:42:06 +0000

    Updated practice files

--- events-in-jquery/script.js
+++ events-in-jquery/script.js
@@ -1,11 +1,79 @@
 $(document).ready(function() {
 
+    // changes color of p tags when clicked
     $("p").click(function(){
         $("p").css('color', 'red');
     });
 
+    // changes color of h2 tags when hovered
     $("h2").hover(function(){
         $("h2").css('color', 'lightblue');
+    });
+
+    // changes font size of particular h2 headings
+    // and changes the other h2 headings back to
+    //original size
+    
+    $("#head-html").hover(function(){
+        $("#head-html").css('font-size', '2em');
+        $("#head-sql").css('font-size', '1em');
+        $("#head-python").css('font-size', '1em');
+        $("#head-jquery").css('font-size', '1em');
+        $("#head-django").css('font-size', '1em');
+        $("#head-css").css('font-size', '1em');
+    });
+
+     $("#head-sql").hover(function(){
+        $("#head-sql").css('font-size', '2em');
+        $("#head-html").css('font-size', '1em');
+        $("#head-python").css('font-size', '1em');
+        $("#head-jquery").css('font-size', '1em');
+        $("#head-django").css('font-size', '1em');
+        $("#head-css").css('font-size', '1em');
+    });
+
+    $("#head-python").hover(function(){
+        $("#head-sql").css('font-size', '1em');
+        $("#head-html").css('font-size', '1em');
+        $("#head-python").css('font-size', '2em');
+        $("#head-jquery").css('font-size', '1em');
+        $("#head-django").css('font-size', '1em');
+        $("#head-css").css('font-size', '1em');
+    });
+
+    $("#head-jquery").hover(function(){
+        $("#head-sql").css('font-size', '1em');
+        $("#head-html").css('font-size', '1em');
+        $("#head-python").css('font-size', '1em');
+        $("#head-jquery").css('font-size', '2em');
+        $("#head-django").css('font-size', '1em');
+        $("#head-css").css('font-size', '1em');
+    });
+
+    $("#head-django").hover(function(){
+        $("#head-sql").css('font-size', '1em');
+        $("#head-html").css('font-size', '1em');
+        $("#head-python").css('font-size', '1em');
+        $("#head-jquery").css('font-size', '1em');
+        $("#head-django").css('font-size', '2em');
+        $("#head-css").css('font-size', '1em');
+    });
+
+    $("#head-css").hover(function(){
+        $("#head-sql").css('font-size', '1em');
+        $("#head-html").css('font-size', '1em');
+        $("#head-python").css('font-size', '1em');
+        $("#head-jquery").css('font-size', '1em');
+        $("#head-django").css('font-size', '1em');
+        $("#head-css").css('font-size', '2em');
+    });
+
+    $(".bottom_button").mouseenter(function() {
+        $("body").css("background-color", "black");
+    });
+
+    $(".bottom_button").mouseleave(function(){
+        $("body").css("background-color", "#eee");
     });
 
 
